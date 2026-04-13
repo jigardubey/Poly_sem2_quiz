a6010a0d75c53dfbecd26a34d8ebe20810627ff8
jigardubey <jigardubey2806@gmail.com>
2026-04-13 18:51:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -630,7 +630,7 @@ if st.session_state.quiz_data:
     st.session_state.current_q += 1
     st.rerun()
 
-        else:
+else:
         st.balloons()
         st.write(f"## 🎉 Quiz Khatam! Tera Score: {st.session_state.score}/{len(q_list)}")
         if st.button("Restart"):
