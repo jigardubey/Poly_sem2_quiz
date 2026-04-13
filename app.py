3c2976bf071bf7eb37e6da02e264b042eb46f352
jigardubey <jigardubey2806@gmail.com>
2026-04-13 18:31:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -623,15 +623,4 @@ if st.session_state.quiz_data:
         if st.button("Restart"):
             st.session_state.quiz_data = []
             st.rerun()
-          # Code ke sabse last mein ye dalo
-st.markdown("---")
-st.markdown(
-    """
-    <div style='text-align: center;'>
-        <p>© 2026 <b>Jigar Dubey</b> | All Rights Reserved</p>
-        <p>Created for Polytechnic Exam Preparation</p>
-    </div>
-    """, 
-    unsafe_allow_stdio=True
-)
-
+          
